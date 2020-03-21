45cacac18f9c5ac89ed611229a303b4034ed2411
艾力亚尔·阿布里米提 <shixi_abulimiti@staff.weibo.com>
2020-03-21 19:38:49 +0800

    [237] 删除链表中的节点

---
+++ 链表/237.删除链表中的节点.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode.cn id=237 lang=python3
+#
+# [237] 删除链表中的节点
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        while node.next:
+            node.val = node.next.val
+            if not node.next.next:
+                node.next = None
+            else:
+                node = node.next
+
+# @lc code=end
