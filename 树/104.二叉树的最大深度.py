2cea4d572729e70fa005db7cb6333b27898b07a2
Elyar <1595685558@qq.com>
2020-03-24 15:41:25 +0800

    [104] 二叉树的最大深度  递归，DFS，BFS实现

--- 树/104.二叉树的最大深度.py
+++ 树/104.二叉树的最大深度.py
@@ -14,12 +14,41 @@
 
 
 class Solution:
+    # def maxDepth(self, root: TreeNode) -> int:
+    #     # 递归实现
+    #     if not root:
+    #         return 0
+    #     left_hegiht = self.maxDepth(root.left)
+    #     right_hegight = self.maxDepth(root.right)
+    #     return max(left_hegiht, right_hegight)+1
+
+    # def maxDepth(self, root: TreeNode) -> int:
+    #     # DFS实现
+    #     stack = []
+    #     if root:
+    #         stack.append((1, root))
+    #     depth = 0
+    #     while len(stack) != 0:
+    #         current_depth, node = stack.pop()
+    #         if node:
+    #             depth = max(current_depth, depth)
+    #             stack.append((current_depth + 1, node.left))
+    #             stack.append((current_depth + 1, node.right))
+    #     return depth
+
     def maxDepth(self, root: TreeNode) -> int:
-        if not root:
-            return 0
-        left_hegiht = self.maxDepth(root.left)
-        right_hegight = self.maxDepth(root.right)
-        return max(left_hegiht, right_hegight)+1
+        # BFS实现
+        stack = []
+        if root:
+            stack.append((1, root))
+        depth = 0
+        while len(stack) != 0:
+            current_depth, node = stack.pop(0)
+            if node:
+                depth = max(current_depth, depth)
+                stack.append((current_depth + 1, node.left))
+                stack.append((current_depth + 1, node.right))
+        return depth
 
 
 # @lc code=end
