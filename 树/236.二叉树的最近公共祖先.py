2e30968bd3a82d1d1707fe6fcbe69e91c4d9359a
Elyar <1595685558@qq.com>
2020-04-08 23:00:49 +0800

    [236] 二叉树的最近公共祖先

---
+++ 树/236.二叉树的最近公共祖先.py
@@ -0,0 +1,39 @@
+#
+# @lc app=leetcode.cn id=236 lang=python3
+#
+# [236] 二叉树的最近公共祖先
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        self.ans = None
+
+        def recurse_tree(current_node):
+
+            # If reached the end of a branch, return False.
+            if not current_node:
+                return False
+
+            left = recurse_tree(current_node.left)
+            right = recurse_tree(current_node.right)
+
+            tmp = current_node == q or current_node == p
+
+            if left + right+tmp >= 2:
+                self.ans = current_node
+            return left or right or tmp
+
+        recurse_tree(root)
+        return self.ans
+
+
+# @lc code=end
