bdd8b705e5deafb84860667148b67cfa22551f6c
Elyar <1595685558@qq.com>
2020-05-20 01:34:15 +0800

    [70] 爬楼梯 添加更好看的写法

--- 动态规划/70.爬楼梯.py
+++ 动态规划/70.爬楼梯.py
@@ -8,17 +8,28 @@
 
 
 class Solution:
+    # def climbStairs(self, n: int) -> int:
+    #     # d[n]=d[n-1]+d[n-2]
+    #     if n <= 2:
+    #         return n
+    #     first = 1
+    #     second = 2
+    #     d = 0
+    #     for i in range(3, n + 1):
+    #         d = first + second
+    #         first, second = second, d
+    #     return d
+
     def climbStairs(self, n: int) -> int:
-        # d[n]=d[n-1]+d[n-2]
         if n <= 2:
             return n
-        first = 1
-        second = 2
-        d = 0
-        for i in range(3, n + 1):
-            d = first + second
-            first, second = second, d
-        return d
+        first, second = 1, 2
+        cur = first + second
+        for i in range(3, n):
+            first, second = second, cur
+            cur = first+second
+
+        return cur
 
 
 # @lc code=end
