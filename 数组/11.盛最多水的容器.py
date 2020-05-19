c7f575c5fdb1a65751ecd4f24389340aa3f8c6e4
Elyar <1595685558@qq.com>
2020-05-20 01:15:15 +0800

    [11] 盛最多水的容器   双指针法

---
+++ 数组/11.盛最多水的容器.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode.cn id=11 lang=python3
+#
+# [11] 盛最多水的容器
+#
+
+# @lc code=start
+
+
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        # 双指针法 时间复杂度 O(n)
+        left, right = 0, len(height) - 1
+        area = 0
+        while left < right:
+            area = max(min(height[left], height[right]) * (right - left), area)
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return area
+
+
+# @lc code=end
