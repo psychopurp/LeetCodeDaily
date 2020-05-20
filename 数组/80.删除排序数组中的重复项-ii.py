17e609abdaec716c041326ac152b2ba700b9bc25
Elyar <1595685558@qq.com>
2020-05-21 01:15:13 +0800

    [80] 删除排序数组中的重复项 II  双指针

---
+++ 数组/80.删除排序数组中的重复项-ii.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode.cn id=80 lang=python3
+#
+# [80] 删除排序数组中的重复项 II
+#
+
+# @lc code=start
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+
+        j = 2
+        for i in range(2, len(nums)):
+            if nums[i] != nums[j - 2]:
+                nums[j] = nums[i]
+                j += 1
+        return j
+
+
+# @lc code=end
