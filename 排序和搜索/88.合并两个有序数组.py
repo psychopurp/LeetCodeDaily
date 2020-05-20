87d3cebe79ef3b84464669bdbce2ca9b596d5549
Elyar <1595685558@qq.com>
2020-05-21 03:08:35 +0800

    [88] 合并两个有序数组  更好的写法

--- 排序和搜索/88.合并两个有序数组.py
+++ 排序和搜索/88.合并两个有序数组.py
@@ -8,23 +8,44 @@
 
 
 class Solution:
+    # def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    #     """
+    #     Do not return anything, modify nums1 in-place instead.
+    #     """
+    #     i = m - 1
+    #     j = n - 1
+    #     k = m + n-1
+
+    #     while i >= 0 and j >= 0:
+    #         if nums1[i] > nums2[j]:
+    #             nums1[k] = nums1[i]
+    #             i -= 1
+    #         else:
+    #             nums1[k] = nums2[j]
+    #             j -= 1
+    #         k -= 1
+    #     nums1[:j+1] = nums2[:j+1]
+
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i = m - 1
-        j = n - 1
-        k = m + n-1
-
-        while i >= 0 and j >= 0:
-            if nums1[i] > nums2[j]:
-                nums1[k] = nums1[i]
-                i -= 1
-            else:
+        # 双指针
+        i, j = m-1, n-1
+        for k in reversed(range(m+n)):
+            if i < 0:
                 nums1[k] = nums2[j]
                 j -= 1
-            k -= 1
-        nums1[:j+1] = nums2[:j+1]
+                continue
+            if j < 0:
+                break
+            if nums1[i] > nums2[j]:
+                val = nums1[i]
+                i -= 1
+            else:
+                val = nums2[j]
+                j -= 1
+            nums1[k] = val
 
 # @lc code=end
 
