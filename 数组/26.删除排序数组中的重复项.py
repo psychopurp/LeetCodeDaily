6a53c08718d19aff091bd0c6dad9d7750d382853
Elyar <1595685558@qq.com>
2020-05-20 16:55:54 +0800

    [26] 删除排序数组中的重复项  添加更直观的解法

--- 数组/26.删除排序数组中的重复项.py
+++ 数组/26.删除排序数组中的重复项.py
@@ -8,16 +8,24 @@
 
 
 class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        slow=0
-        for fast, k in enumerate(nums):
-            if nums[slow] == nums[fast]:
-                continue
-            else:
-                slow+=1
-                nums[slow]=nums[fast]
-        return slow+1
+    # def removeDuplicates(self, nums: List[int]) -> int:
+    #     slow=0
+    #     for fast, k in enumerate(nums):
+    #         if nums[slow] == nums[fast]:
+    #             continue
+    #         else:
+    #             slow+=1
+    #             nums[slow]=nums[fast]
+    #     return slow+1
 
-                
-            
+    def removeDuplicates(self, nums: List[int]) -> int:
+        j = 0
+        for k in range(1, len(nums)):
+
+            if nums[k] != nums[j]:
+                j += 1
+                nums[j] = nums[k]
+        return j + 1
+
+
 # @lc code=end
