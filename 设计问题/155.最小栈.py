c7646ef5cb14d4b0fc642394a65b87fcab75dd57
Elyar <1595685558@qq.com>
2020-05-21 11:12:45 +0800

    [155] 最小栈 添加只用一个栈的写法

--- 设计问题/155.最小栈.py
+++ 设计问题/155.最小栈.py
@@ -41,3 +41,32 @@ class MinStack:
         # param_3 = obj.top()
         # param_4 = obj.getMin()
         # @lc code=end
+
+
+'''
+class MinStack:
+    
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack=[]
+
+
+    def push(self, x: int) -> None:
+        if not self.stack:
+            self.stack.append((x,x))
+        else:
+            self.stack.append((x,min(x,self.getMin())))
+            
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
+
+'''
