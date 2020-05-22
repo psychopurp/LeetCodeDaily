3ca2ebb229e54485679bc320f344822393b15559
Elyar <1595685558@qq.com>
2020-05-22 09:38:50 +0800

    [144] 二叉树的前序遍历

---
+++ 树/144.二叉树的前序遍历.py
@@ -0,0 +1,31 @@
+#
+# @lc app=leetcode.cn id=144 lang=python3
+#
+# [144] 二叉树的前序遍历
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def preorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        stack = [root]
+        res = []
+        while stack:
+            node = stack.pop()
+            if node:
+                res.append(node.val)
+                stack.append(node.right)
+                stack.append(node.left)
+        return res
+
+
+# @lc code=end
