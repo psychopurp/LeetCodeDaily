aa0e5f511d679ef9a7c50937ae386e85eb5110d9
Elyar <1595685558@qq.com>
2020-05-23 14:28:02 +0800

    [70] 爬楼梯 添加递归+备忘录 解法

--- 动态规划/70.爬楼梯.py
+++ 动态规划/70.爬楼梯.py
@@ -20,16 +20,28 @@ class Solution:
     #         first, second = second, d
     #     return d
 
+    # def climbStairs(self, n: int) -> int:
+    #     if n <= 2:
+    #         return n
+    #     first, second = 1, 2
+    #     cur = first + second
+    #     for i in range(3, n):
+    #         first, second = second, cur
+    #         cur = first+second
+
+    #     return cur
+
     def climbStairs(self, n: int) -> int:
-        if n <= 2:
-            return n
-        first, second = 1, 2
-        cur = first + second
-        for i in range(3, n):
-            first, second = second, cur
-            cur = first+second
+        # 递归加备忘录 时间复杂度O(N) 空间复杂度O(N)
+        def helper(memo, n):
+            if n <= 2:
+                return n
+            if memo[n - 1] != 0:
+                return memo[n-1]
+            memo[n-1] = helper(memo, n - 1) + helper(memo, n - 2)
+            return memo[n-1]
 
-        return cur
-
+        dp_table = [0]*n
+        return helper(dp_table, n)
 
 # @lc code=end
