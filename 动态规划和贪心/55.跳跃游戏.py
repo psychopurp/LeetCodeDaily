6c64850f6a5a468f81f72113728c35e62dce5696
Elyar <1595685558@qq.com>
2020-05-29 14:27:49 +0800

    [55] 跳跃游戏 添加另一种写法

--- 动态规划和贪心/55.跳跃游戏.py
+++ 动态规划和贪心/55.跳跃游戏.py
@@ -8,13 +8,24 @@
 
 
 class Solution:
-    def canJump(self, nums: List[int]) -> bool:
+    # def canJump(self, nums: List[int]) -> bool:
 
-        right = 0
-        for i in range(len(nums)):
-            if i <= right:
-                right = max(right, i + nums[i])
-                if right >= len(nums) - 1:
-                    return True
-        return False
+    #     right = 0
+    #     for i in range(len(nums)):
+    #         if i <= right:
+    #             right = max(right, i + nums[i])
+    #             if right >= len(nums) - 1:
+    #                 return True
+    #     return False
+
+    def canJump(self, nums: List[int]) -> bool:
+        # 从后开始遍历
+        last = len(nums) - 1
+        i = last - 1
+        while i >= 0:
+            if i + nums[i] >= last:
+                last = i
+            i -= 1
+        return last == 0
+
         # @lc code=end
