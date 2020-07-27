2e84de4a0a171f458248b81da92ab4f23332f1fb
Elyar~ <1595685558@qq.com>
2020-07-27 14:35:27 +0800

    添加两种partition方法

--- 一些排序算法实现/quick_sort.py
+++ 一些排序算法实现/quick_sort.py
@@ -9,25 +9,42 @@ def generate_rand(num):
     return result
 
 
+def partition_left(nums, low, high):
+    # 取左边第一位
+    '''high为最后一个元素下标，函数返回基准值'''
+    pivot = nums[low]
+    while low < high:
+        # 从右边找比它小的
+        while low < high and nums[high] >= pivot:
+            high -= 1
+        nums[low] = nums[high]
+        # 从左边找比它大的
+        while low < high and nums[low] <= pivot:
+            low += 1
+        nums[high] = nums[low]
+    nums[low] = pivot
+    return low
+
+
+def partition_right(nums, low, high):
+    # 取左边第一位
+    '''high为最后一个元素下标，函数返回基准值'''
+    pivot = nums[high]
+    while low < high:
+        while low < high and nums[low] <= pivot:
+            low += 1
+        nums[high] = nums[low]
+        while low < high and nums[high] >= pivot:
+            high -= 1
+        nums[low] = nums[high]
+    nums[low] = pivot
+    return low
+
+
 def quick_sort(nums, low, high):
     '''时间复杂度 O(nlog n)'''
-    # 返回基准值
-    def partition(nums, low, high):
-        pivot = nums[low]
-        while low < high:
-            # 从右边找比它小的
-            while low < high and nums[high] >= pivot:
-                high -= 1
-            nums[low] = nums[high]
-            # 从左边找比它大的
-            while low < high and nums[low] <= pivot:
-                low += 1
-            nums[high] = nums[low]
-        nums[low] = pivot
-        return low
-
     if low < high:
-        base = partition(nums, low, high)
+        base = partition_right(nums, low, high)
         quick_sort(nums, low, base - 1)
         quick_sort(nums, base+1, high)
 
@@ -49,6 +66,6 @@ def quick_sort_2(nums):
 
 if __name__ == "__main__":
     data = generate_rand(1000)
-    # quick_sort(data, 0, len(data) - 1)
-    result = quick_sort_2(data)
-    print(result)
+    quick_sort(data, 0, len(data) - 1)
+    # result = quick_sort_2(data)
+    print(data)
