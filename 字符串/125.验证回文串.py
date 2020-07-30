14003dc2945b46c7d364a7e97efd4c8036543be5
Elyar~ <1595685558@qq.com>
2020-07-30 14:07:50 +0800

    [125] 验证回文串 添加更清晰的写法

--- 字符串/125.验证回文串.py
+++ 字符串/125.验证回文串.py
@@ -8,23 +8,39 @@
 
 
 class Solution:
+    # def isPalindrome(self, s: str) -> bool:
+    #     low, high = 0, len(s) - 1
+    #     gap = ord('a')-ord('A')
+    #     while low < high:
+    #         if not self.check(s[low]):
+    #             low += 1
+    #             continue
+    #         if not self.check(s[high]):
+    #             high -= 1
+    #             continue
+    #         if (('0' <= s[low] <= '9') and (s[high] > '9' or s[high] < '0')) or (('0' <= s[high] <= '9') and (s[low] > '9' or s[low] < '0')):
+    #             return False
+    #         if s[low] == s[high] or (ord(s[low]) + gap) == ord(s[high]) or (ord(s[low]) - gap) == ord(s[high]):
+    #             low += 1
+    #             high -= 1
+    #         else:
+    #             return False
+    #     return True
+
     def isPalindrome(self, s: str) -> bool:
         low, high = 0, len(s) - 1
-        gap = ord('a')-ord('A')
+
         while low < high:
-            if not self.check(s[low]):
+            if not s[low].isalnum():
                 low += 1
                 continue
-            if not self.check(s[high]):
+            if not s[high].isalnum():
                 high -= 1
                 continue
-            if (('0' <= s[low] <= '9') and (s[high] > '9' or s[high] < '0')) or (('0' <= s[high] <= '9') and (s[low] > '9' or s[low] < '0')):
+            if s[low].lower() != s[high].lower():
                 return False
-            if s[low] == s[high] or (ord(s[low]) + gap) == ord(s[high]) or (ord(s[low]) - gap) == ord(s[high]):
-                low += 1
-                high -= 1
-            else:
-                return False
+            low += 1
+            high -= 1
         return True
 
     def check(self, char):
