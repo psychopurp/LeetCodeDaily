a16831e866d51eedc8059cbb8ff0397e14b29358
Elyar~ <1595685558@qq.com>
2020-08-04 20:31:13 +0800

    [191] 位1的个数 修改更简洁的写法

--- 数学/191.位-1-的个数.py
+++ 数学/191.位-1-的个数.py
@@ -6,22 +6,21 @@
 
 # @lc code=start
 class Solution:
-    # def hammingWeight(self, n: int) -> int:
-    #     total = 0
-    #     mask = 1
-    #     for i in range(32):
-    #         if mask & n != 0:
-    #             total += 1
-    #         mask = mask << 1
-    #     return total
-
     def hammingWeight(self, n: int) -> int:
-        # 对于任意一个数，将 n 和 n-1 进行 & 运算，我们都可以把 n 中最低位的 1 变成 0
         total = 0
-        while n > 0:
-            n &= n - 1
-            total += 1
+        for i in range(32):
+            if n & 1 != 0:
+                total += 1
+            n = n >> 1
         return total
+
+    # def hammingWeight(self, n: int) -> int:
+    #     # 对于任意一个数，将 n 和 n-1 进行 & 运算，我们都可以把 n 中最低位的 1 变成 0
+    #     total = 0
+    #     while n > 0:
+    #         n &= n - 1
+    #         total += 1
+    #     return total
 
 
 # @lc code=end
