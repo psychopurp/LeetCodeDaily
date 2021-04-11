c662e69ee9e9055a0ff06c05fe82a5a91f2fa9ff
Elyar~ <1595685558@qq.com>
2021-04-11 22:35:53 +0800

    [227] 基本计算器 II 添加双栈写法

--- 栈/227.基本计算器-ii.py
+++ 栈/227.基本计算器-ii.py
@@ -53,29 +53,80 @@ class Solution:
     #             stack.append(int(i))
     #     return sum(stack)
 
+    # def calculate(self, s: str) -> int:
+    #     # 时间复杂度 O(N) 空间复杂度 O(N)
+
+    #     n = len(s)
+    #     stack = []
+    #     preSign = "+"
+    #     num = 0
+
+    #     for i in range(n):
+    #         if s[i] != " " and s[i].isdigit():
+    #             num = num*10+ord(s[i])-ord('0')
+    #         if s[i] in "+-*/" or i == n-1:
+    #             if preSign == "+":
+    #                 stack.append(num)
+    #             elif preSign == '-':
+    #                 stack.append(-num)
+    #             elif preSign == "*":
+    #                 stack.append(stack.pop()*num)
+    #             else:
+    #                 stack.append(int(stack.pop()/num))
+    #             preSign = s[i]
+    #             num = 0
+    #     return sum(stack)
+
     def calculate(self, s: str) -> int:
-        # 时间复杂度 O(N) 空间复杂度 O(N)
+        # 双栈写法
+        priority = {
+            "+": 1,
+            "-": 1,
+            "*": 2,
+            "/": 2
+        }
+        operations = {
+            "+": lambda x, y: x+y,
+            "-": lambda x, y: x-y,
+            "*": lambda x, y: x*y,
+            "/": lambda x, y: int(x/y),
+        }
 
-        n = len(s)
-        stack = []
-        preSign = "+"
-        num = 0
+        opors = []
+        oprands = []
 
-        for i in range(n):
-            if s[i] != " " and s[i].isdigit():
-                num = num*10+ord(s[i])-ord('0')
-            if s[i] in "+-*/" or i == n-1:
-                if preSign == "+":
-                    stack.append(num)
-                elif preSign == '-':
-                    stack.append(-num)
-                elif preSign == "*":
-                    stack.append(stack.pop()*num)
-                else:
-                    stack.append(int(stack.pop()/num))
-                preSign = s[i]
+        i, n = 0, len(s)
+        while i < n:
+            # 过滤空格
+            if s[i] == " ":
+                while i < n and s[i] == " ":
+                    i += 1
+
+            elif s[i].isdigit():
+                # 取数字
                 num = 0
-        return sum(stack)
+                while i < n and s[i].isdigit():
+                    num = num*10+int(s[i])
+                    i += 1
+                oprands.append(num)
+
+            else:
+                while opors and priority[s[i]] <= priority[opors[-1]]:
+                    y = oprands.pop()
+                    x = oprands.pop()
+                    op = opors.pop()
+                    oprands.append(operations[op](x, y))
+                else:
+                    opors.append(s[i])
+                i += 1
+
+        while opors:
+            y = oprands.pop()
+            x = oprands.pop()
+            op = opors.pop()
+            oprands.append(operations[op](x, y))
+
+        return oprands.pop()
 
 
 # @lc code=end
