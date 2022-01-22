1d5e7cc92781ab04fdab2d5bc96c47138365b2d3
psychopurp <1595685558@qq.com>
2022-01-22 18:27:04 +0800

    [15] 3Sum : add sort&two-pointer solution

---
+++ array/15.3-sum.py
@@ -0,0 +1,50 @@
+#
+# @lc app=leetcode.cn id=15 lang=python3
+#
+# [15] 3Sum
+#
+
+# @lc code=start
+
+
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # time complexity: O(n^2)
+        # spcae complexity: O(n)
+
+        nums.sort()
+        n = len(nums)
+        result = []
+
+        for i in range(n):
+            if nums[i] > 0:
+                break
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+
+            left, right = i + 1, n - 1
+
+            while left < right:
+                val = nums[i] + nums[left] + nums[right]
+                if val == 0:
+                    result.append([nums[i], nums[left], nums[right]])
+
+                    right -= 1
+                    while left < right and nums[right] == nums[right+1]:
+                        right -= 1
+                    left += 1
+                    while left < right and nums[left] == nums[left-1]:
+                        left += 1
+
+                elif val > 0:
+                    right -= 1
+                else:
+                    left += 1
+
+        return result
+
+
+# @lc code=end
