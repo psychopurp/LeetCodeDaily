6ed73631db681208e7ab713423c1bfb1090d1852
psychopurp <1595685558@qq.com>
2022-04-15 17:51:11 +0800

    [78] Subsets : add general approach

---
+++ backtracking/78.subsets.py
@@ -0,0 +1,63 @@
+#
+# @lc app=leetcode.cn id=78 lang=python3
+#
+# [78] Subsets
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    # def subsets(self, nums: List[int]) -> List[List[int]]:
+    #     # backtracking
+    #     # space complexity: O(n)
+    #     # time complexity: O(n*2^n)
+
+    #     def backtrack(way: List[int], index: int):
+    #         ans.append(way[:])
+
+    #         for i in range(index, len(nums)):
+    #             way.append(nums[i])
+    #             backtrack(way, i+1)
+    #             way.pop()
+
+    #     ans = []
+    #     backtrack([], 0)
+    #     return ans
+
+    # def subsets(self, nums: List[int]) -> List[List[int]]:
+    #     # Iterative
+    #     # space complexity: O(n)
+    #     # time complexity: O(n*2^n)
+
+    #     ans = [[]]
+
+    #     for num in nums:
+
+    #         newSubset = []
+    #         for subset in ans:
+    #             newSubset.append(subset+[num])
+
+    #         ans.extend(newSubset)
+
+    #     return ans
+
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        # bit manipulation
+        # space complexity: O(n)
+        # time complexity: O(n*2^n)
+
+        ans = []
+
+        i = 0
+        while i < (1 << len(nums)):
+            subset = []
+            for j in range(len(nums)):
+                if i >> j & 1:
+                    subset.append(nums[j])
+            ans.append(subset[:])
+            i += 1
+        return ans
+
+# @lc code=end
