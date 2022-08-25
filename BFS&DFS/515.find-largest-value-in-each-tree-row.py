aba1fb1a516ae277940de292186bd522ba09f243
psychopurp <44772640+psychopurp@users.noreply.github.com>
2022-08-25 02:15:57 +0000

    [515] Find Largest Value in Each Tree Row : add BFS solution

--- BFS&DFS/515.find-largest-value-in-each-tree-row.py
+++ BFS&DFS/515.find-largest-value-in-each-tree-row.py
@@ -17,28 +17,58 @@ class TreeNode:
 
 
 class Solution:
+    # def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+    #     # DFS: iterative
+    #     # time complexity: O(N)
+    #     # space complexity: O(N)
+
+    #     if not root:
+    #         return []
+
+    #     stack = []
+    #     stack.append((root, 0))
+    #     result = []
+
+    #     while stack:
+    #         node, level = stack.pop()
+    #         if len(result) == level:
+    #             result.append(node.val)
+    #         else:
+    #             result[level] = max(result[level], node.val)
+    #         if node.left:
+    #             stack.append((node.left, level+1))
+    #         if node.right:
+    #             stack.append((node.right, level+1))
+
+    #     return result
+
     def largestValues(self, root: Optional[TreeNode]) -> List[int]:
-        # DFS: iterative
+        # BFS: level order traversal
         # time complexity: O(N)
         # space complexity: O(N)
 
+        from collections import deque
         if not root:
             return []
 
-        stack = []
-        stack.append((root, 0))
-        result = []
+        q = deque([root])
+        ans = []
+        level = 0
+        while q:
+            if len(ans) == level:
+                ans.append(q[0].val)
 
-        while stack:
-            node, level = stack.pop()
-            if len(result) == level:
-                result.append(node.val)
-            else:
-                result[level] = max(result[level], node.val)
-            if node.left:
-                stack.append((node.left, level+1))
-            if node.right:
-                stack.append((node.right, level+1))
+            n = len(q)
+            for _ in range(n):
+                node = q.popleft()
+                ans[level] = max(ans[level], node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
 
-        return result
+            level += 1
+
+        return ans
+
 # @lc code=end
