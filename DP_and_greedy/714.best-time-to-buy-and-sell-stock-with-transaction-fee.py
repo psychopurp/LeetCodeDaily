661add80f0ae1e54992e6c791d3382d6e4eb4f13
psychopurp <44772640+psychopurp@users.noreply.github.com>
2023-09-21 13:56:14 +0800

    [714] Best Time to Buy and Sell Stock with Transaction Fee: add bottom-up dp

--- DP_and_greedy/714.best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ DP_and_greedy/714.best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -9,24 +9,44 @@ from typing import List
 
 
 class Solution:
+    # def maxProfit(self, prices: List[int], fee: int) -> int:
+    #     # DP
+    #     # time complexity: O(N)
+    #     # space complexity: O(1)
+    #     '''
+    #     For every day, we've three choices:
+    #     1.Don't do anything : cur_hold=pre_hold, cur_not_hold=pre_not_hold
+    #     2.Sell them for a profit : cur_not_hold=pre_hold+price
+    #     3.Buy new stock : cur_hold=pre_not_hold-price
+    #     '''
+
+    #     cur_hold, cur_not_hold = float("-inf"), 0
+
+    #     for price in prices:
+    #         pre_hold, pre_not_hold = cur_hold, cur_not_hold
+
+    #         cur_hold = max(pre_hold, pre_not_hold-price)
+    #         cur_not_hold = max(pre_not_hold, pre_hold+price-fee)
+
+    #     return cur_not_hold
+
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        # DP
+        # Bottom-up DP
         # time complexity: O(N)
-        # space complexity: O(1)
-        '''
-        For every day, we've three choices:
-        1.Don't do anything : cur_hold=pre_hold, cur_not_hold=pre_not_hold
-        2.Sell them for a profit : cur_not_hold=pre_hold+price
-        3.Buy new stock : cur_hold=pre_not_hold-price
-        '''
+        # space complexity: O(N)
 
-        cur_hold, cur_not_hold = float("-inf"), 0
+        n = len(prices)
 
-        for price in prices:
-            pre_hold, pre_not_hold = cur_hold, cur_not_hold
+        # [day][whether hold stock]
+        dp = [[0, 0] for _ in range(n)]
 
-            cur_hold = max(pre_hold, pre_not_hold-price)
-            cur_not_hold = max(pre_not_hold, pre_hold+price-fee)
+        dp[0][1] = -prices[0]
 
-        return cur_not_hold
+        for i in range(1, n):
+            dp[i][1] = max(dp[i - 1][1], dp[i - 1][0] - prices[i])
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee)
+
+        return dp[-1][0]
+
+
 # @lc code=end
