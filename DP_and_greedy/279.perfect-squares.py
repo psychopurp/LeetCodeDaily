dff86c0c31ff0395c7261564b3d3e6b9c4e459f4
psychopurp <44772640+psychopurp@users.noreply.github.com>
2023-09-21 19:14:16 +0800

    [279] Perfect Squares: add new DP solution

--- DP_and_greedy/279.perfect-squares.py
+++ DP_and_greedy/279.perfect-squares.py
@@ -44,31 +44,50 @@ class Solution:
 
     #     return dp(n)
 
+    # def numSquares(self, n: int) -> int:
+    #     # BFS
+    #     # time complexity: O(n*sqrt(n))
+    #     # space complexity: O(n)
+
+    #     from collections import deque
+
+    #     visited = set()
+    #     q = deque()
+    #     q.append((0, 1))
+
+    #     while q:
+    #         for _ in range(len(q)):
+    #             u, depth = q.popleft()
+    #             for i in range(1, n+1):
+    #                 v = u+i*i
+    #                 if v == n:
+    #                     return depth
+
+    #                 if v > n:
+    #                     break
+
+    #                 if v not in visited:
+    #                     visited.add(v)
+    #                     q.append((v, depth+1))
+
     def numSquares(self, n: int) -> int:
-        # BFS
+        # Bottom-up DP, same as coin change
         # time complexity: O(n*sqrt(n))
         # space complexity: O(n)
 
-        from collections import deque
+        nums = []
+        for i in range(n + 1):
+            if i * i > n:
+                break
+            nums.append(i * i)
 
-        visited = set()
-        q = deque()
-        q.append((0, 1))
+        dp = [i for i in range(n + 1)]
 
-        while q:
-            for _ in range(len(q)):
-                u, depth = q.popleft()
-                for i in range(1, n+1):
-                    v = u+i*i
-                    if v == n:
-                        return depth
-
-                    if v > n:
-                        break
-
-                    if v not in visited:
-                        visited.add(v)
-                        q.append((v, depth+1))
+        for i in range(n + 1):
+            for num in nums:
+                if i + num <= n:
+                    dp[i + num] = min(dp[i + num], dp[i] + 1)
+        return dp[-1]
 
 
 # @lc code=end
