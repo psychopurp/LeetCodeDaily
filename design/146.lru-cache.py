ab00a914b900d740d135d4a33be99b9afa714b7e
psychopurp <44772640+psychopurp@users.noreply.github.com>
2023-09-29 11:27:23 +0800

    [146] LRU Cache: implement lru cache

---
+++ design/146.lru-cache.py
@@ -0,0 +1,82 @@
+#
+# @lc app=leetcode.cn id=146 lang=python3
+#
+# [146] LRU Cache
+#
+
+# @lc code=start
+from typing import Dict, Optional
+
+
+class LRUCache:
+    class CacheItem:
+        def __init__(self, key: int, val: int) -> None:
+            self.key = key
+            self.val = val
+            self.prev: Optional[LRUCache.CacheItem] = None
+            self.next: Optional[LRUCache.CacheItem] = None
+
+    def __init__(self, capacity: int):
+        self.cache: Dict[int, LRUCache.CacheItem] = {}
+        self.capacity = capacity
+        self.head: LRUCache.CacheItem = LRUCache.CacheItem(0, 0)
+        self.tail: LRUCache.CacheItem = LRUCache.CacheItem(0, 0)
+
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+
+        item = self.cache[key]
+        self.move_node_to_head(item)
+        return item.val
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            node = self.cache[key]
+            node.val = value
+            self.move_node_to_head(node)
+            return
+
+        if len(self.cache) == self.capacity:
+            self.evict(self.tail.prev)
+
+        item = LRUCache.CacheItem(key, value)
+        self.cache[key] = item
+        self.move_node_to_head(item)
+
+    def evict(self, cache_item: CacheItem):
+        self.take_node_out(cache_item)
+        self.cache.pop(cache_item.key)
+
+    def take_node_out(self, cache_item: CacheItem):
+        if cache_item.prev:
+            cache_item.prev.next = cache_item.next
+            cache_item.next.prev = cache_item.prev
+
+    def move_node_to_tail(self, cache_item: CacheItem):
+        self.take_node_out(cache_item)
+
+        cache_item.prev = self.tail.prev
+        cache_item.next = self.tail
+
+        self.tail.prev.next = cache_item
+        self.tail.prev = cache_item
+
+    def move_node_to_head(self, cache_item: CacheItem):
+        self.take_node_out(cache_item)
+
+        cache_item.prev = self.head
+        cache_item.next = self.head.next
+
+        self.head.next.prev = cache_item
+        self.head.next = cache_item
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+# @lc code=end
