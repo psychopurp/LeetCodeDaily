41885ecfc399e435e8803cc40cc61f2ba614a361
psychopurp <44772640+psychopurp@users.noreply.github.com>
2023-10-08 14:55:42 +0800

    [53] Maximum Subarray: add brute-force solution

--- DP_and_greedy/53.maximum-subarray.py
+++ DP_and_greedy/53.maximum-subarray.py
@@ -10,6 +10,40 @@ from typing import List
 
 class Solution:
     # def maxSubArray(self, nums: List[int]) -> int:
+    #     # 1.Brute force solution: whis will MLE
+    #     # time complexity: O(N^2)
+    #     # space complexity: O(N^2)
+    #     n = len(nums) + 1
+
+    #     dp = [[0 for _ in range(n)] for _ in range(n)]
+    #     max_val = float("-inf")
+
+    #     for i in range(1, n):
+    #         dp[i][i] = nums[i - 1]
+
+    #     for i in range(1, n):
+    #         for j in range(i, n):
+    #             dp[i][j] = dp[i][j - 1] + nums[j - 1]
+    #             max_val = max(max_val, dp[i][j])
+
+    #     return max_val
+
+    # def maxSubArray(self, nums: List[int]) -> int:
+    #     # 2.DP optimization from brute force
+    #     # time complexity: O(N)
+    #     # space complexity: O(N)
+    #     n = len(nums) + 1
+
+    #     dp = [0 for _ in range(n)]
+    #     max_val = float("-inf")
+
+    #     for i in range(1, n):
+    #         dp[i] = max(nums[i - 1], dp[i - 1] + nums[i - 1])
+    #         max_val = max(max_val, dp[i])
+
+    #     return max_val
+
+    # def maxSubArray(self, nums: List[int]) -> int:
     #     # Bottom-up DP :
     #     # time complexity: O(N)
     #     # space complexity: O(1)
@@ -27,7 +61,7 @@ class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         # Top-down DP : dp[i] represents largest sum till i
         # time complexity: O(N)
-        # space complexity: O(1)
+        # space complexity: O(N) stack usage
 
         def dp(i: int):
             nonlocal max_val
@@ -35,11 +69,13 @@ class Solution:
             if i == 0:
                 val = nums[i]
             else:
-                val = max(nums[i], dp(i-1)+nums[i])
+                val = max(nums[i], dp(i - 1) + nums[i])
             max_val = max(max_val, val)
             return val
+
         max_val = float("-inf")
-        dp(len(nums)-1)
+        dp(len(nums) - 1)
         return max_val
+
 
 # @lc code=end
