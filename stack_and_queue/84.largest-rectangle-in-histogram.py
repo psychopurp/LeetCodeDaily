9797aa7010fdcc8ef6d485db7b8b125c09269861
psychopurp <44772640+psychopurp@users.noreply.github.com>
2023-10-26 16:05:36 +0800

    [84] Largest Rectangle in Histogram: Brute Force and Monotonic Stack

--- stack_and_queue/84.largest-rectangle-in-histogram.py
+++ stack_and_queue/84.largest-rectangle-in-histogram.py
@@ -4,58 +4,73 @@
 # [84] Largest Rectangle in Histogram
 #
 
+
 # @lc code=start
+from typing import List
+
+
 class Solution:
     # def largestRectangleArea(self, heights: List[int]) -> int:
-    #     # brute force solution 1
-    #     # time complexity: O(n^2)
+    #     # 1.Brute-force: iterate from left to right. This will TLE!
+    #     # time complexity: O(N^2)
     #     # space complexity: O(1)
-    #     maxArea = 0
-    #     for i in range(len(heights)):
-    #         minHeight = heights[i]
-    #         for j in range(i, len(heights)):
-    #             minHeight = min(minHeight, heights[j])
-    #             maxArea = max(maxArea, (j+1-i)*minHeight)
-    #     return maxArea
+
+    #     n = len(heights)
+    #     max_val = 0
+
+    #     for r in range(n):
+    #         min_height = heights[r]
+    #         for l in range(r, -1, -1):
+    #             min_height = min(min_height, heights[l])
+    #             width = r - l + 1
+    #             max_val = max(max_val, min_height * width)
+
+    #     return max_val
 
     # def largestRectangleArea(self, heights: List[int]) -> int:
-    #     # brute force solution 2
-    #     # time complexity: O(n^2)
+    #     # 2.Brute-force: iterate from down to up. This will TLE!
+    #     # time complexity: O(N^2)
     #     # space complexity: O(1)
 
-    #     maxArea = 0
-    #     for i in range(len(heights)):
-    #         curHeight = heights[i]
+    #     n = len(heights)
+    #     max_val = 0
 
-    #         # find left boundary
-    #         left = i
-    #         while left-1 >= 0 and heights[left-1] >= curHeight:
-    #             left -= 1
+    #     for i in range(n):
+    #         h = heights[i]
 
-    #         # find right boundary
-    #         right = i
-    #         while right+1 < len(heights) and heights[right+1] >= curHeight:
-    #             right += 1
-    #         maxArea = max(maxArea, (right-left+1)*curHeight)
-    #     return maxArea
+    #         # find the left and right boundary
+    #         l = r = i
+    #         while l >= 0 and heights[l] >= h:
+    #             l -= 1
+
+    #         while r < n and heights[r] >= h:
+    #             r += 1
+
+    #         max_val = max(max_val, (r - l - 1) * h)
+
+    #     return max_val
 
     def largestRectangleArea(self, heights: List[int]) -> int:
-        # Monotone stack solution
-        # time complexity: O(n)
-        # space complexity: O(n)
-        maxArea = 0
+        # 3.Monotonic stack
+        # time complexity: O(N)
+        # space complexity: O(N)
+
         heights.append(0)
+        n = len(heights)
+        max_val = 0
+
         stack = []
 
-        for i in range(len(heights)):
-            while stack and heights[i] < heights[stack[-1]]:
-                curHeight = heights[stack.pop()]
-                right = i
-                left = stack[-1] if stack else -1
-                maxArea = max(maxArea, (right-left-1)*curHeight)
+        for i in range(n):
+            r = i
+            while stack and heights[stack[-1]] > heights[i]:
+                h = heights[stack.pop()]
+                l = stack[-1] if stack else -1
+                max_val = max(max_val, (r - l - 1) * h)
 
             stack.append(i)
 
-        return maxArea
+        return max_val
+
 
 # @lc code=end
