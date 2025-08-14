1b007f6778454c69ec830271d9cf57ae4c56dabc
psychopurp <psychopurp@gmail.com>
2025-08-15 00:23:30 +0800

    [11] Container With Most Water: add brute force solution

--- hash_set_array/11.container-with-most-water.py
+++ hash_set_array/11.container-with-most-water.py
@@ -6,6 +6,18 @@
 
 # @lc code=start
 class Solution:
+
+    # def maxArea(self, height: List[int]) -> int:
+    #     # brute force
+    #     # time complexity O(n^2)
+    #     # space complexity O(1)
+    #     n = len(height)
+    #     area = 0
+    #     for i in range(n):
+    #         for j in range(i + 1, n):
+    #             area = max(area, min(height[i], height[j]) * (j - i), area)
+    #     return area
+
     def maxArea(self, height: List[int]) -> int:
         # 双指针法 时间复杂度 O(n)
         left, right = 0, len(height) - 1
