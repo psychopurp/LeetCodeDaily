f7f56499f27fcb146183017dce9886f07a9b317d
psychopurp <psychopurp@gmail.com>
2025-08-30 14:36:24 +0800

    [146] LRU Cache: add more concise solution

--- design/146.lru-cache.py
+++ design/146.lru-cache.py
@@ -8,6 +8,14 @@
 from typing import Dict, Optional
 
 
+class Node:
+    def __init__(self, key: int, value: int) -> None:
+        self.key = key
+        self.val = value
+        self.prev: Optional[Node] = None
+        self.next: Optional[Node] = None
+
+
 class LRUCache:
     """
     solution 1 : implement double-ended linked list
@@ -82,31 +90,80 @@ class LRUCache:
     solution 2 : using ordered dict
     """
 
-    def __init__(self, capacity: int):
-        from collections import OrderedDict
+    # def __init__(self, capacity: int):
+    #     from collections import OrderedDict
 
-        self.cache = OrderedDict()
+    #     self.cache = OrderedDict()
+    #     self.capacity = capacity
+
+    # def get(self, key: int) -> int:
+    #     if key not in self.cache:
+    #         return -1
+
+    #     # key as the newest one
+    #     v = self.cache.pop(key)
+    #     self.cache[key] = v
+    #     return v
+
+    # def put(self, key: int, value: int) -> None:
+    #     if key in self.cache:
+    #         self.cache.pop(key)
+    #         self.cache[key] = value
+    #         return
+
+    #     if len(self.cache) == self.capacity:
+    #         self.cache.popitem(last=False)
+
+    #     self.cache[key] = value
+
+    """
+    solution 3 : more concise solution
+    """
+
+    def __init__(self, capacity: int):
+        self.cache: Dict[int, Node] = {}
         self.capacity = capacity
+        self.size = 0
+        self.head: Node = Node(0, 0)
+        self.tail: Node = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def move_node_to_head(self, node: Node):
+        self.take_node_out(node)
+        self.insert_node_to_head(node)
+
+    def take_node_out(self, node: Node):
+        node.prev.next, node.next.prev = node.next, node.prev
+        self.size -= 1
+        self.cache.pop(node.key)
+
+    def insert_node_to_head(self, node: Node):
+        self.head.next.prev, self.head.next, node.prev, node.next = (
+            node,
+            node,
+            self.head,
+            self.head.next,
+        )
+        self.size += 1
+        self.cache[node.key] = node
 
     def get(self, key: int) -> int:
-        if key not in self.cache:
-            return -1
-
-        # key as the newest one
-        v = self.cache.pop(key)
-        self.cache[key] = v
-        return v
+        if key in self.cache:
+            node = self.cache[key]
+            self.move_node_to_head(node)
+            return node.val
+        return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.cache.pop(key)
-            self.cache[key] = value
-            return
-
-        if len(self.cache) == self.capacity:
-            self.cache.popitem(last=False)
-
-        self.cache[key] = value
+        if key not in self.cache:
+            if self.size == self.capacity:
+                self.take_node_out(self.tail.prev)
+            self.insert_node_to_head(Node(key, value))
+        else:
+            node = self.cache[key]
+            node.val = value
+            self.move_node_to_head(node)
 
 
 # Your LRUCache object will be instantiated and called as such:
